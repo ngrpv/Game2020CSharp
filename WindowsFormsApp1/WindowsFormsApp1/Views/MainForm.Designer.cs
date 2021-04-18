9edb4bf1e4852d9bb7b72aea1c25a88afeb6accf
Nazar Garipov <nazar.garipov475@gmail.com>
2021-04-18 21:30:50 +0500

    Add options

--- WindowsFormsApp1/WindowsFormsApp1/Views/MainForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Views/MainForm.Designer.cs
@@ -25,43 +25,39 @@ namespace WindowsFormsApp1.Views
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
-        {
-            this.menuControl1 = new WindowsFormsApp1.Views.MenuControl();
-            this.optionsControl1 = new WindowsFormsApp1.Views.OptionsControl();
-            this.SuspendLayout();
-            // 
-            // menuControl1
-            // 
-            this.menuControl1.Location = new System.Drawing.Point(152, 94);
-            this.menuControl1.Name = "menuControl1";
-            this.menuControl1.Size = new System.Drawing.Size(441, 219);
-            this.menuControl1.TabIndex = 0;
-            this.menuControl1.Load += new System.EventHandler(this.menuControl1_Load);
-            // 
-            // optionsControl1
-            // 
-            this.optionsControl1.Location = new System.Drawing.Point(81, 33);
-            this.optionsControl1.Name = "optionsControl1";
-            this.optionsControl1.Size = new System.Drawing.Size(557, 346);
-            this.optionsControl1.TabIndex = 1;
-            this.optionsControl1.Visible = false;
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 461);
-            this.Controls.Add(this.optionsControl1);
-            this.Controls.Add(this.menuControl1);
-            this.Name = "MainForm";
-            this.Text = "MainForm";
-            this.ResumeLayout(false);
-        }
+{
+    this.menuControl1 = new WindowsFormsApp1.Views.MenuControl();
+    this.optionsControl1 = new WindowsFormsApp1.Views.OptionsControl();
+    this.SuspendLayout();
+    // 
+    // menuControl1
+    // 
+    this.menuControl1.Location = new System.Drawing.Point(152, 94);
+    this.menuControl1.Name = "menuControl1";
+    this.menuControl1.Size = new System.Drawing.Size(441, 219);
+    this.menuControl1.TabIndex = 0;
+    this.menuControl1.Load += new System.EventHandler(this.menuControl1_Load);
+    // 
+    // optionsControl1
+    // 
+    this.optionsControl1.Location = new System.Drawing.Point(81, 33);
+    this.optionsControl1.Name = "optionsControl1";
+    this.optionsControl1.Size = new System.Drawing.Size(557, 346);
+    this.optionsControl1.TabIndex = 1;
+    this.optionsControl1.Visible = false;
+    // 
+    // MainForm
+    // 
+    this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    this.ClientSize = new System.Drawing.Size(800, 461);
+    this.Controls.Add(this.optionsControl1);
+    this.Controls.Add(this.menuControl1);
+    this.Name = "MainForm";
+    this.Text = "MainForm";
+    this.ResumeLayout(false);
+}
 
         private WindowsFormsApp1.Views.OptionsControl optionsControl1;
 
